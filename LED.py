abf54b07cd99bb57f8d3531f8c05676f924ab4d4
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2021-12-29 17:28:26 -0500

    Added comment on how GIFT coords work.

--- LED.py
+++ LED.py
@@ -2,4 +2,7 @@ class LED:
     def __init__(self, ID, pos, RGB):
         self.ID = ID  # int from 0 to LED_NUM-1 inclusive
         self.pos = pos  # tuple in the form (x, y, z)
+                        # X, Y and Z are in GIFT format
+                        # X and Y are between -1 and 1 inclusive, with 0 being the trunk of the tree
+                        # Z is from 0 to 1 inclusive, with 0 being the bottom of the tree
         self.RGB = RGB  # tuple in the form (R, G, B), where all 3 values are ints from 0 to 255 inclusive
