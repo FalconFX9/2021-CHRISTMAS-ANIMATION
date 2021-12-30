904d09ce8a5407ba64e759db09cb96f79fdc77e2
zant2f <foot@bell.net>
2021-12-29 21:07:43 -0500

    Closest LED
    Checks for closest LED if there are no new infected LEDs.

--- main.py
+++ main.py
@@ -42,24 +42,38 @@ def random_colors():
 def pandemic_sim():
     for led in LED_LIST:
         led.RGB = (0, 255, 0)
-
     led = random.choice(LED_LIST)
     led.RGB = (255, 0, 0)
     file = open("pandemic.csv", 'w', encoding='utf-8', newline='')
     writer = csv.writer(file, quotechar='|', quoting=csv.QUOTE_MINIMAL)
     write_first_line(writer)
     infected_LEDs = [led]
-    for i in range(1000):
+
+    for i in range(1500):
         if i % 20 == 0 and len(infected_LEDs) < 500:
+            not_infected_LEDs = [led for led in LED_LIST if led not in infected_LEDs]
             new_infects = []
             for led in infected_LEDs:
                 a, b, c = led.pos
                 for led_2 in LED_LIST:
                     x, y, z = led_2.pos
-                    r = 0.3
+                    r = 0.2
                     if (x - a)**2 + (y - b)**2 + (z - c)**2 < r **2 and led_2 not in infected_LEDs:
                         led_2.RGB = (255, 0, 0)
                         new_infects.append(led_2)
+                    if not new_infects:
+                        closest_led = not_infected_LEDs[0]
+                        sd = ((a * closest_led.pos[0])**2 + (b * closest_led.pos[1])**2 + (c * closest_led.pos[2])**2)**(1/2)
+                        for led_n in not_infected_LEDs:
+                            x2, y2, z2 = led_n.pos
+                            shortest_distance = ((a * x2)**2 + (b * y2)**2 + (c * z2)**2)**(1/2)
+                            if sd > shortest_distance:
+                                sd = shortest_distance
+                                closest_led = led_n
+                        closest_led.RGB = (255, 0, 0)
+                        new_infects.append(closest_led)
+
+                        
             new_infects = list(set(new_infects))
             infected_LEDs.extend(new_infects)
 
